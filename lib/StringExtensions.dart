928bc1f4577398c93a4a88a08342fdd1b6ecfa78
faveoled <85760289+faveoled@users.noreply.github.com>
2024-02-26 10:27:58 -0800

    Images

--- lib/StringExtensions.dart
+++
@@ -1,9 +0,0 @@
-extension StringExtensions on String {
-  String withoutTrailingSlash() {
-    if (endsWith('/') || endsWith('\\')) {
-      return substring(0, length - 1);
-    } else {
-      return this;
-    }
-  }
-}
